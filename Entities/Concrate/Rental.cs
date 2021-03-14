d599e7686f5d26a51d788755a1550ec0b5858f0a
BerkaySimseek <berkaysimseek@outlook.com>
2021-03-15 02:07:50 +0300

    Updated

--- Entities/Concrate/Rental.cs
+++ Entities/Concrate/Rental.cs
@@ -12,7 +12,7 @@ namespace Entities.Concrate
         public int RentId { get; set; }
         public int CarId { get; set; }
         public int CustomerId { get; set; }
-        public int RentDate { get; set; }
-        public int ReturnDate { get; set; }
+        public DateTime? RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
     }
 }
